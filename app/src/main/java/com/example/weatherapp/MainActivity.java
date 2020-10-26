a6dd51396d0ce1858c7c5758f8c5a2afd0cd81df
Alina_D <alinadorozhkina91@mail.ru>
2020-10-26 19:45:34 +0300

    Прогноз погоды, начальный интерфейс

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView weatherDescription;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        weatherDescription=findViewById(R.id.textViewResult);
+    }
+
+    public void showMoscowWeather (View v){
+        String description = getResources().getString(R.string.moscow_weather);
+        weatherDescription.setText(description);
+
+    }
+    public void showRomaWeather (View v){
+        String description = getResources().getString(R.string.roma_weather);
+        weatherDescription.setText(description);
+
+    }
+    public void showTokyoWeather (View v){
+        String description = getResources().getString(R.string.tokyo_weather);
+        weatherDescription.setText(description);
+
+    }
+    public void showLosAngelesWeather (View v){
+        String description = getResources().getString(R.string.los_angeles_weather);
+        weatherDescription.setText(description);
+
     }
 }
